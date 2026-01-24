7345b03ded312a08858e1f4fe87a6a50e62f0a52
Filipe Conde <fconde-p@student.42sp.org.br>
2026-01-24 20:21:32 -0300

    chore: create file for mnishell headers.

---
+++ minishell.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fconde-p <fconde-p@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/29 16:55:46 by fconde-p          #+#    #+#             */
+/*   Updated: 2026/01/24 20:10:49 by fconde-p         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include "./lib/libft/libft.h"
+# include <unistd.h>
+# include <fcntl.h>
+# include <stdlib.h>
+
+// def funny stuff here, friendo!
+int	ft_sample(void);
+
+#endif
