96ccc8d54a23ac5103ca194229bd011678f86ea7
Filipe Conde <fconde-p@student.42sp.org.br>
2026-01-25 17:08:29 -0300

    chore: add new sample files definitions.

--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: fconde-p <fconde-p@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/29 16:55:46 by fconde-p          #+#    #+#             */
-/*   Updated: 2026/01/24 20:10:49 by fconde-p         ###   ########.fr       */
+/*   Updated: 2026/01/25 15:37:00 by fconde-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,7 @@
 
 // def funny stuff here, friendo!
 int	ft_sample(void);
+int	ft_sample_fail(void);
+int	ft_sample_success(void);
 
 #endif
