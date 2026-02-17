0d5b0c5fe882a98b342c0579169ec0b6b765c645
Filipe Conde <fconde-p@student.42sp.org.br>
2026-02-17 16:38:33 -0300

    chore: add comments to file.

--- tests/tests.h
+++ tests/tests.h
@@ -2,14 +2,19 @@
 #ifndef TESTS_H
 # define TESTS_H
 
+// project header files
 # include "../lib/libft/libft.h"
 # include "../minishell.h"
+
+// other header files
 # include <unistd.h>
 # include <fcntl.h>
 # include <stdlib.h>
 # include <stdio.h>
 
 // def funny stuff here, friendo!
+
+// MACRO for individual test running and result printing
 #define RUN_TEST(test_func) \
 	if (test_func() != 0) { \
 		printf("      [FAIL] Case: %s\n", #test_func); \
