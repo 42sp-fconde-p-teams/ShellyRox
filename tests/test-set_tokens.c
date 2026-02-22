588b7df2490c55b65ab650125423e51ae7d12601
Filipe Conde <fconde-p@student.42sp.org.br>
2026-02-22 13:26:25 -0300

    chore: separa testes da função set_tokens() com comentários.

--- tests/test-set_tokens.c
+++ tests/test-set_tokens.c
@@ -52,29 +52,11 @@ int	create_new_node_with_null_prev_and_next(void)
 	}
 }
 
-int	put_value_to_two_new_token_nodes(void)
-{
-	char	str_in[] = "teste";
-	t_token	*token = NULL;
-
-	token = set_tokens(str_in);
-	if (!token)
-		return (EXIT_FAILURE);
-	if (ft_strncmp(token->value, str_in, 6) == EXIT_SUCCESS)
-	{
-		free(token);
-		return (EXIT_SUCCESS);
-	}
-	else
-	{
-		free(token);
-		return (EXIT_FAILURE);
-	}
-}
-
 int	main(void)
 {
+	// simple token creation tests
 	RUN_TEST(put_value_to_a_new_token_node);
 	RUN_TEST(create_new_node_with_null_prev_and_next);
+	// token list tests
 	return (0);
 }
