4a430b0a51c85b670d309e1209d635c846c26140
Filipe Conde <fconde-p@student.42sp.org.br>
2026-02-27 21:59:42 -0300

    test: reforça testes de set_tokens() básico.

--- tests/test-set_tokens.c
+++ tests/test-set_tokens.c
@@ -22,6 +22,36 @@ int	put_value_to_a_new_token_node(void)
 	}
 }
 
+int	return_null_for_empty_token(void)
+{
+	char	str_in[] = "";
+	t_token	*token = NULL;
+
+	token = set_tokens(str_in);
+	if (!token)
+		return (EXIT_SUCCESS);
+	else
+	{
+		free(token);
+		return (EXIT_FAILURE);
+	}
+}
+
+int	return_null_for_only_spaces_token(void)
+{
+	char	str_in[] = "    ";
+	t_token	*token = NULL;
+
+	token = set_tokens(str_in);
+	if (!token)
+		return (EXIT_SUCCESS);
+	else
+	{
+		free(token);
+		return (EXIT_FAILURE);
+	}
+}
+
 int	create_new_node_with_null_prev_and_next(void)
 {
 	char	str_in[] = "token";
@@ -76,6 +106,28 @@ int	create_list_with_three_tokens(void)
 	}
 }
 
+int	create_list_ignoring_spaces(void)
+{
+	char	str_in[] = "    token size   test     ";
+	t_token	*token = NULL;
+
+	token = set_tokens(str_in);
+	if (!token)
+		return (EXIT_FAILURE);
+	if (ft_strncmp(token->value, "token", 5) == EXIT_SUCCESS
+		&& ft_strncmp(token->next->value, "size", 4) == EXIT_SUCCESS
+		&& ft_strncmp(token->next->next->value, "test", 4) == EXIT_SUCCESS)
+	{
+		free(token);
+		return (EXIT_SUCCESS);
+	}
+	else
+	{
+		free(token);
+		return (EXIT_FAILURE);
+	}
+}
+
 int	set_correctly_reverse_link_for_list_of_three(void)
 {
 	char	str_in[] = "token size test";
@@ -104,8 +156,11 @@ int	main(void)
 	// simple token creation tests
 	RUN_TEST(put_value_to_a_new_token_node);
 	RUN_TEST(create_new_node_with_null_prev_and_next);
-	RUN_TEST(create_list_with_three_tokens);
-	RUN_TEST(set_correctly_reverse_link_for_list_of_three);
+	RUN_TEST(return_null_for_empty_token);
+	RUN_TEST(return_null_for_only_spaces_token);
 	// token list tests
+	RUN_TEST(create_list_with_three_tokens);
+	RUN_TEST(create_list_ignoring_spaces);
+	RUN_TEST(set_correctly_reverse_link_for_list_of_three);
 	return (0);
 }
