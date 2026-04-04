e5b7a3670d85d12c7eb9af08cdb962954904c646
Filipe Conde <filipecondep@gmail.com>
2026-04-04 00:48:05 -0300

    feat: cria função para liberar memória de array de strings e funções para liberar memória após realizar strjoin().

--- lib/libft/ft_strjoin.c
+++ lib/libft/ft_strjoin.c
@@ -35,3 +35,27 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	ft_strlcat(ptr, s2, s1_size + s2_size + 1);
 	return (ptr);
 }
+
+char	*ft_strjoin_and_free(char *s1, char *s2)
+{
+	char	*new_str;
+
+	new_str = ft_strjoin(s1, s2);
+	free(s1);
+	return (new_str);
+}
+
+char	*ft_strjoin_char_and_free(char *s1, char c)
+{
+	char	*char_str;
+	char	*new_str;
+
+	char_str = (char *)ft_calloc(2, sizeof(char));
+	if (!char_str)
+		return (NULL);
+	char_str[0] = c;
+	new_str = ft_strjoin(s1, char_str);
+	free(s1);
+	free(char_str);
+	return (new_str);
+}
