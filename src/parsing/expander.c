e8317f3b7f06abafdf616287407c75c242926cd3
Filipe Conde <filipecondep@gmail.com>
2026-04-05 15:35:12 -0300

    refactor: reduz funções do expander.c para tamanho permitido pela norma (precisa quebrar em mais arquivos).

--- src/parsing/expander.c
+++ src/parsing/expander.c
@@ -6,7 +6,7 @@
 /*   By: fconde-p <fconde-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/03 18:00:00 by fconde-p          #+#    #+#             */
-/*   Updated: 2026/04/03 18:00:00 by fconde-p         ###   ########.fr       */
+/*   Updated: 2026/04/05 11:27:02 by fconde-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,14 +21,14 @@ static char	*remove_quotes(char *value, t_bool *quoted_flag)
 	*quoted_flag = BOOL_FALSE;
 	if (!value)
 		return (NULL);
-	i = 0;
-	j = 0;
 	new_value = ft_calloc(ft_strlen(value) + 1, sizeof(char));
 	if (!new_value)
 		return (NULL);
+	i = 0;
+	j = 0;
 	while (value[i])
 	{
-		if ((value[i] == '\"' || value[i] == '\''))
+		if (value[i] == '"' || value[i] == '\'')
 		{
 			*quoted_flag = BOOL_TRUE;
 			i++;
@@ -41,50 +41,53 @@ static char	*remove_quotes(char *value, t_bool *quoted_flag)
 	return (new_value);
 }
 
+static char	*get_var_value(char *var_name, t_shelly *shelly)
+{
+	if (ft_strncmp(var_name, "?", 1) == 0)
+		return (ft_itoa(shelly->last_exit_status));
+	return (get_env_value(var_name, shelly->envp));
+}
+
+static int	find_var_end(char *value, int start)
+{
+	if (value[start] == '?')
+		return (start + 1);
+	while (value[start] && (ft_isalnum(value[start]) || value[start] == '_'))
+		start++;
+	return (start);
+}
+
+static int	process_variable(char *value, int i, t_shelly *shelly, char **res)
+{
+	int		start;
+	char	*var_name;
+	char	*var_value;
+
+	start = find_var_end(value, i + 1);
+	var_name = ft_substr(value, i + 1, start - (i + 1));
+	var_value = get_var_value(var_name, shelly);
+	if (var_value)
+		*res = ft_strjoin_and_free(*res, var_value);
+	free(var_name);
+	free(var_value);
+	return (start);
+}
+
 static char	*expand_variables(char *value, t_shelly *shelly, t_bool is_quoted)
 {
 	char	*expanded_str;
 	int		i;
-	int		start;
-	char	*var_name;
-	char	*var_value;
 
 	expanded_str = ft_strdup("");
 	i = 0;
 	while (value[i])
 	{
-		// Only expand if NOT single-quoted. 
-		// If is_quoted is true and starts with single quote, we skip expansion.
 		if (value[i] == '$' && value[i + 1] && \
-			(ft_isalnum(value[i + 1]) || value[i + 1] == '_' || value[i + 1] == '?') && \
-			(!is_quoted || value[0] == '\"'))
-		{
-			start = i + 1;
-			if (value[start] == '?')
-				start++;
-			else
-			{
-				while (value[start] && (ft_isalnum(value[start]) || value[start] == '_'))
-					start++;
-			}
-			var_name = ft_substr(value, i + 1, start - (i + 1));
-			if (ft_strncmp(var_name, "?", 1) == 0)
-				var_value = ft_itoa(shelly->last_exit_status);
-			else
-				var_value = get_env_value(var_name, shelly->envp);
-			if (var_value)
-			{
-				expanded_str = ft_strjoin_and_free(expanded_str, var_value);
-				free(var_value);
-			}
-			free(var_name);
-			i = start;
-		}
+			(ft_isalnum(value[i + 1]) || value[i + 1] == '_' || \
+			value[i + 1] == '?') && (!is_quoted || value[0] == '"'))
+			i = process_variable(value, i, shelly, &expanded_str);
 		else
-		{
-			expanded_str = ft_strjoin_char_and_free(expanded_str, value[i]);
-			i++;
-		}
+			expanded_str = ft_strjoin_char_and_free(expanded_str, value[i++]);
 	}
 	return (expanded_str);
 }
@@ -111,67 +114,66 @@ static char	*expand_tilde(char *value, t_shelly *shelly)
 
 void	insert_new_tokens(t_token **head, t_token *current, char **words)
 {
-	t_token *new_token;
-	t_token *temp;
+	t_token	*new_token;
+	t_token	*temp;
 	int		i;
 
-	i = 1; // words[0] is already in current->value
+	i = 1;
 	while (words[i])
 	{
 		new_token = ft_calloc(1, sizeof(t_token));
 		if (!new_token)
-			return;
+			return ;
 		new_token->value = ft_strdup(words[i]);
 		new_token->type = TOKEN_WORD;
 		new_token->quoted = BOOL_FALSE;
-		
 		temp = current->next;
 		current->next = new_token;
 		new_token->prev = current;
 		new_token->next = temp;
 		if (temp)
 			temp->prev = new_token;
-		
 		current = new_token;
 		i++;
 	}
 	(void)head;
 }
 
-t_token	*expander(t_token *tokens, t_shelly *shelly)
+static void	handle_word_expansion(t_token *current, t_shelly *shelly, t_token **tokens)
 {
-	t_token	*current;
 	char	*temp_value;
 	t_bool	quoted_flag;
 	char	**split_words;
+
+	temp_value = expand_tilde(current->value, shelly);
+	free(current->value);
+	current->value = temp_value;
+	quoted_flag = current->quoted;
+	temp_value = expand_variables(current->value, shelly, quoted_flag);
+	free(current->value);
+	current->value = temp_value;
+	if (!quoted_flag && ft_strchr(current->value, ' '))
+	{
+		split_words = ft_split(current->value, ' ');
+		free(current->value);
+		current->value = ft_strdup(split_words[0]);
+		insert_new_tokens(tokens, current, split_words);
+		ft_free_array(split_words);
+	}
+	temp_value = remove_quotes(current->value, &quoted_flag);
+	current->quoted = quoted_flag;
+	current->value = temp_value;
+}
+
+t_token	*expander(t_token *tokens, t_shelly *shelly)
+{
+	t_token	*current;
 
 	current = tokens;
 	while (current)
 	{
 		if (current->type == TOKEN_WORD)
-		{
-			temp_value = expand_tilde(current->value, shelly);
-			free(current->value);
-			current->value = temp_value;
-
-			quoted_flag = current->quoted;
-			temp_value = expand_variables(current->value, shelly, quoted_flag);
-			free(current->value);
-			current->value = temp_value;
-
-			if (!quoted_flag && ft_strchr(current->value, ' '))
-			{
-				split_words = ft_split(current->value, ' ');
-				free(current->value);
-				current->value = ft_strdup(split_words[0]);
-				insert_new_tokens(&tokens, current, split_words);
-				ft_free_array(split_words);
-			}
-
-			temp_value = remove_quotes(current->value, &quoted_flag);
-			current->quoted = quoted_flag;
-			current->value = temp_value;
-		}
+			handle_word_expansion(current, shelly, &tokens);
 		current = current->next;
 	}
 	return (tokens);
