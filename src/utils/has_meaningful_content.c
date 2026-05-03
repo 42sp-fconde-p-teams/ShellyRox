e0a18af143c7b7a311f6b3b20dc4d0e2ce591279
Filipe Conde <fconde-p@student.42sp.org.br>
2026-05-03 20:30:35 -0300

    fix: sanitiza entradas do projeto para não quebrar com espaços vazios, tabs e quebras de linha, inclusive combinados.

--- src/utils/has_meaningful_content.c
+++ src/utils/has_meaningful_content.c
@@ -6,15 +6,32 @@
 /*   By: fconde-p <fconde-p@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/03 17:18:39 by fconde-p          #+#    #+#             */
-/*   Updated: 2026/05/03 17:21:42 by fconde-p         ###   ########.fr       */
+/*   Updated: 2026/05/03 20:09:21 by fconde-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../minishell.h"
 
+static int	has_only_invisible_chars(char *line)
+{
+	int	i;
+
+	i = 0;
+	while (line[i] != '\0')
+	{
+		if (line[i] == ' ' || line[i] == '\t' || line[i] == '\n')
+			i++;
+		else
+			return (EXIT_FAILURE);
+	}
+	return (EXIT_SUCCESS);
+}
+
 int		has_meaningful_content(char *line)
 {
 	if (line[0] == '\0')
+		return (EXIT_FAILURE);
+	if (has_only_invisible_chars(line) == EXIT_SUCCESS)
 		return (EXIT_FAILURE);
 	return (EXIT_SUCCESS);
 }
