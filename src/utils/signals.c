95860f9c43c95cac0846d62c146e42c5d43ba95c
Filipe Conde <fconde-p@student.42sp.org.br>
2026-05-09 14:40:41 -0300

    feat: implementa seleção de configuração para tratamento de sinais iniciais.

--- src/utils/signals.c
+++ src/utils/signals.c
@@ -6,7 +6,7 @@
 /*   By: fconde-p <fconde-p@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/01 00:00:00 by csilva-s          #+#    #+#             */
-/*   Updated: 2026/05/09 13:42:24 by fconde-p         ###   ########.fr       */
+/*   Updated: 2026/05/09 14:39:47 by fconde-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,8 +32,21 @@ void	setup_signals(t_sig_state state)
 	struct sigaction	sa;
 
 	ft_bzero(&sa, sizeof(struct sigaction));
-	sa.sa_handler = handle_sigint;
-	sigaction(SIGINT, &sa, NULL);
-	sa.sa_handler = SIG_IGN;
-	sigaction(SIGQUIT, &sa, NULL);
+	if (state == SIG_STATE_INTERACTIVE)
+	{
+		sa.sa_handler = handle_sigint;
+		sigaction(SIGINT, &sa, NULL);
+		sa.sa_handler = SIG_IGN;
+		sigaction(SIGQUIT, &sa, NULL);
+	} else if (state == SIG_STATE_CHILD)
+	{
+		sa.sa_handler = SIG_DFL;
+		sigaction(SIGINT, &sa, NULL);
+		sigaction(SIGQUIT, &sa, NULL);
+	} else if (state == SIG_STATE_IGNORE)
+	{
+		sa.sa_handler = SIG_IGN;
+		sigaction(SIGINT, &sa, NULL);
+		sigaction(SIGQUIT, &sa, NULL);
+	}
 }
