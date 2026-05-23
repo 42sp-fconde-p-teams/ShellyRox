c40f3b13306d5c69ab9fbbe15cb52cd02bc39bc6
Filipe Conde <fconde-p@student.42sp.org.br>
2026-05-23 19:02:18 -0300

    refactor: cria função auxiliar para distribuir lógica da função.

--- src/utils/env_array.c
+++ src/utils/env_array.c
@@ -3,44 +3,56 @@
 /*                                                        :::      ::::::::   */
 /*   env_array.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fconde-p <fconde-p@student.42.fr>          +#+  +:+       +#+        */
+/*   By: fconde-p <fconde-p@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/12 21:00:00 by fconde-p          #+#    #+#             */
-/*   Updated: 2026/04/15 00:10:00 by fconde-p         ###   ########.fr       */
+/*   Updated: 2026/05/23 19:00:33 by fconde-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../minishell.h"
 
+static char **set_env_var_array(t_env *curr, char *temp_val, int count)
+{
+	int i;
+	char	**array;
+
+	i = 0;
+	array = NULL;
+	array = malloc(sizeof(char *) * (count + 1));
+	if (!array)
+		return (NULL);
+	while (curr)
+	{
+		temp_val = ft_strjoin("=", curr->value);
+		if (!temp_val)
+			return (NULL);
+		array[i++] = ft_strjoin(curr->key, temp_val);
+		free(temp_val);
+		curr = curr->next;
+	}
+	array[count] = NULL;
+	return (array);
+}
+
 char	**get_env_array(t_shelly *shell)
 {
 	t_env	*curr;
 	int		count;
-	int		i;
 	char	**array;
 	char	*temp_val;
 
 	count = 0;
+	temp_val = NULL;
 	curr = shell->env_list;
 	while (curr)
 	{
 		count++;
 		curr = curr->next;
 	}
-	array = malloc(sizeof(char *) * (count + 1));
+	curr = shell->env_list;
+	array = set_env_var_array(curr, temp_val, count);
 	if (!array)
 		return (NULL);
-	i = 0;
-	curr = shell->env_list;
-	while (curr)
-	{
-		temp_val = ft_strjoin("=", curr->value);
-		if (!temp_val)
-			return (NULL); // Simplified error handling for brevity
-		array[i++] = ft_strjoin(curr->key, temp_val);
-		free(temp_val);
-		curr = curr->next;
-	}
-	array[i] = NULL;
 	return (array);
 }
