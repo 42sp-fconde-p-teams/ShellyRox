1c0d0bae7c32cfe27331859427540f59a23187a0
Filipe Conde <fconde-p@student.42sp.org.br>
2026-05-23 15:52:43 -0300

    refactor: aplica norma42 aos arquivos sem mudar os comportamentos (pelo menos eu espero).

--- src/parsing/expander/expander.c
+++ src/parsing/expander/expander.c
@@ -3,40 +3,41 @@
 /*                                                        :::      ::::::::   */
 /*   expander.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fconde-p <fconde-p@student.42.fr>          +#+  +:+       +#+        */
+/*   By: fconde-p <fconde-p@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/03 18:00:00 by fconde-p          #+#    #+#             */
-/*   Updated: 2026/04/15 00:43:47 by fconde-p         ###   ########.fr       */
+/*   Updated: 2026/05/23 15:49:46 by fconde-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../minishell.h"
 
-static void	handle_word_expansion(t_token *current, t_shelly *shelly, t_token **tokens)
+static void	handle_word_expansion(t_token *curr,
+	t_shelly *shelly, t_token **tokens)
 {
 	char	*temp_value;
 	t_bool	quoted_flag;
 	char	**split_words;
 
-	temp_value = expand_tilde(current->value, shelly);
-	free(current->value);
-	current->value = temp_value;
-	quoted_flag = current->quoted;
-	temp_value = expand_variables(current->value, shelly, quoted_flag);
-	free(current->value);
-	current->value = temp_value;
-	if (!quoted_flag && !ft_strchr(current->value, '\"') 
-		&& !ft_strchr(current->value, '\'') && ft_strchr(current->value, ' '))
+	temp_value = expand_tilde(curr->value, shelly);
+	free(curr->value);
+	curr->value = temp_value;
+	quoted_flag = curr->quoted;
+	temp_value = expand_variables(curr->value, shelly, quoted_flag);
+	free(curr->value);
+	curr->value = temp_value;
+	if (!quoted_flag && !ft_strchr(curr->value, '\"')
+		&& !ft_strchr(curr->value, '\'') && ft_strchr(curr->value, ' '))
 	{
-		split_words = ft_split(current->value, ' ');
-		free(current->value);
-		current->value = ft_strdup(split_words[0]);
-		insert_new_tokens(tokens, current, split_words);
+		split_words = ft_split(curr->value, ' ');
+		free(curr->value);
+		curr->value = ft_strdup(split_words[0]);
+		insert_new_tokens(tokens, curr, split_words);
 		ft_free_array(split_words);
 	}
-	temp_value = remove_quotes(current->value, &quoted_flag);
-	current->quoted = quoted_flag;
-	current->value = temp_value;
+	temp_value = remove_quotes(curr->value, &quoted_flag);
+	curr->quoted = quoted_flag;
+	curr->value = temp_value;
 }
 
 t_token	*expander(t_token *tokens, t_shelly *shelly)
