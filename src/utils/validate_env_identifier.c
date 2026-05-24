28b61efe2ef2884dbc36eb178456e405df849578
Filipe Conde <fconde-p@student.42sp.org.br>
2026-05-23 21:38:23 -0300

    refactor: limpa função para adequar à norminette.

--- src/utils/validate_env_identifier.c
+++ src/utils/validate_env_identifier.c
@@ -3,9 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   validate_env_identifier.c                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fconde-p <fconde-p@student.42.fr>          +#+  +:+       +#+        */
+/*   By: fconde-p <fconde-p@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/04/16 22:15:00 by fconde-p         ###   ########.fr       */
+/*   Created: 2026/05/23 21:23:46 by fconde-p          #+#    #+#             */
+/*   Updated: 2026/05/23 21:25:31 by fconde-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -13,7 +14,7 @@
 
 int	validate_env_identifier(char *str)
 {
-	int		i;
+	int	i;
 
 	i = 0;
 	if (!str || !str[i])
@@ -22,10 +23,10 @@ int	validate_env_identifier(char *str)
 		return (0);
 	while (str[i])
 	{
-		if (!((str[i] >= 'a' && str[i] <= 'z') || 
-			  (str[i] >= 'A' && str[i] <= 'Z') || 
-			  (str[i] >= '0' && str[i] <= '9') || 
-			  (str[i] == '_')))
+		if (!((str[i] >= 'a' && str[i] <= 'z')
+				|| (str[i] >= 'A' && str[i] <= 'Z')
+				|| (str[i] >= '0' && str[i] <= '9')
+				|| (str[i] == '_')))
 			return (0);
 		i++;
 	}
