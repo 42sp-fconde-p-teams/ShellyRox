c0ec6ba4e6b2088a6a47b4a357a2f7d51a9146fd
Filipe Conde <filipecondep@gmail.com>
2026-05-30 17:21:40 -0300

    typo: ajusta largura de arquivo.

--- src/parsing/fsm/input_checker.c
+++ src/parsing/fsm/input_checker.c
@@ -6,7 +6,7 @@
 /*   By: fconde-p <fconde-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/24 18:28:49 by fconde-p          #+#    #+#             */
-/*   Updated: 2026/05/30 17:16:37 by fconde-p         ###   ########.fr       */
+/*   Updated: 2026/05/30 17:20:27 by fconde-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,8 +30,8 @@ int	input_checker(t_token *tokens)
 		{
 			if (!curr->next || curr->next->type == TOKEN_PIPE)
 			{
-				printf("syntax error near unexpected \
-					token `%s'\n", curr->value);
+				printf("syntax error near unexpected token `%s'\n", 
+					curr->value);
 				return (EXIT_FAILURE);
 			}
 		}
