0e85db0d858c035699b40ba32d20674c375dd37b
Filipe Conde <filipecondep@gmail.com>
2026-05-30 17:57:13 -0300

    refactor: ajusta input_checker à norma42.

--- src/parsing/fsm/input_checker.c
+++ src/parsing/fsm/input_checker.c
@@ -6,7 +6,7 @@
 /*   By: fconde-p <fconde-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/24 18:28:49 by fconde-p          #+#    #+#             */
-/*   Updated: 2026/05/30 17:20:27 by fconde-p         ###   ########.fr       */
+/*   Updated: 2026/05/30 17:38:38 by fconde-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,29 @@
 
 static int	is_redir(t_token_type type)
 {
-	return (type == TOKEN_REDIR_OUT || type == TOKEN_REDIR_IN || \
+	return (type == TOKEN_REDIR_OUT || type == TOKEN_REDIR_IN ||
 		type == TOKEN_HEREDOC || type == TOKEN_APPEND);
+}
+
+static int	print_syntax_error(t_token *token)
+{
+	printf("syntax error near unexpected token `%s'\n",
+		token->value);
+	return (EXIT_FAILURE);
+}
+
+static int	is_invalid_pipe(t_token *curr)
+{
+	if (curr->type != TOKEN_PIPE)
+		return (0);
+	return (!curr->next || curr->next->type == TOKEN_PIPE);
+}
+
+static int	is_invalid_redir(t_token *curr)
+{
+	if (!is_redir(curr->type))
+		return (0);
+	return (!curr->next || curr->next->type != TOKEN_WORD);
 }
 
 int	input_checker(t_token *tokens)
@@ -25,25 +46,13 @@ int	input_checker(t_token *tokens)
 	if (!tokens)
 		return (EXIT_SUCCESS);
 	if (tokens->type == TOKEN_PIPE)
-	{
-		printf("syntax error near unexpected token `%s'\n", tokens->value);
-		return (EXIT_FAILURE);
-	}
+		return (print_syntax_error(tokens));
 	curr = tokens;
 	while (curr)
 	{
-		if (curr->type == TOKEN_PIPE && (!curr->next || curr->next->type == TOKEN_PIPE))
-		{
-			printf("syntax error near unexpected token `%s'\n", curr->value);
-			return (EXIT_FAILURE);
-		}
-		if (is_redir(curr->type) && (!curr->next || curr->next->type != TOKEN_WORD))
-		{
-			printf("syntax error near unexpected token `%s'\n", curr->value);
-			return (EXIT_FAILURE);
-		}
+		if (is_invalid_pipe(curr) || is_invalid_redir(curr))
+			return (print_syntax_error(curr));
 		curr = curr->next;
 	}
 	return (EXIT_SUCCESS);
 }
-
