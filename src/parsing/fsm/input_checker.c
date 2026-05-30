de232fe811c87c632b38b59d4edb3568ea15f35e
Filipe Conde <filipecondep@gmail.com>
2026-05-30 17:18:41 -0300

    feat: implementa comportamento para falha em outros casos de pipe inválido.

--- src/parsing/fsm/input_checker.c
+++ src/parsing/fsm/input_checker.c
@@ -6,7 +6,7 @@
 /*   By: fconde-p <fconde-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/24 18:28:49 by fconde-p          #+#    #+#             */
-/*   Updated: 2026/05/30 17:01:39 by fconde-p         ###   ########.fr       */
+/*   Updated: 2026/05/30 17:16:37 by fconde-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,28 @@
 
 int	input_checker(t_token *tokens)
 {
-	if (ft_strncmp(tokens->value, "|", 1) == 0)
+	t_token	*curr;
+
+	if (!tokens)
+		return (EXIT_SUCCESS);
+	if (tokens->type == TOKEN_PIPE)
 	{
 		printf("syntax error near unexpected token `%s'\n", tokens->value);
 		return (EXIT_FAILURE);
+	}
+	curr = tokens;
+	while (curr)
+	{
+		if (curr->type == TOKEN_PIPE)
+		{
+			if (!curr->next || curr->next->type == TOKEN_PIPE)
+			{
+				printf("syntax error near unexpected \
+					token `%s'\n", curr->value);
+				return (EXIT_FAILURE);
+			}
+		}
+		curr = curr->next;
 	}
 	return (EXIT_SUCCESS);
 }
--- tests/test-input_checker.c
+++ tests/test-input_checker.c
@@ -3,21 +3,76 @@
 
 int	shoud_fail_for_pipe_only_prompt(void)
 {
+	t_token tokens = {"|", TOKEN_PIPE, BOOL_FALSE, NULL, NULL};
 
-	t_token tokens;
-
-	tokens.value = "|";
-	tokens.next = NULL;
-	tokens.prev = NULL;
-	tokens.type = TOKEN_PIPE;
-	if (input_checker((&tokens)) == EXIT_FAILURE)
+	if (input_checker(&tokens) == EXIT_FAILURE)
 		return (EXIT_SUCCESS);
-	else
-		return (EXIT_FAILURE);
+	return (EXIT_FAILURE);
+}
+
+int	should_fail_for_pipe_at_start(void)
+{
+	t_token t1 = {"|", TOKEN_PIPE, BOOL_FALSE, NULL, NULL};
+	t_token t2 = {"ls", TOKEN_WORD, BOOL_FALSE, NULL, NULL};
+
+	t1.next = &t2;
+	t2.prev = &t1;
+	if (input_checker(&t1) == EXIT_FAILURE)
+		return (EXIT_SUCCESS);
+	return (EXIT_FAILURE);
+}
+
+int	should_fail_for_pipe_at_end(void)
+{
+	t_token t1 = {"ls", TOKEN_WORD, BOOL_FALSE, NULL, NULL};
+	t_token t2 = {"|", TOKEN_PIPE, BOOL_FALSE, NULL, NULL};
+
+	t1.next = &t2;
+	t2.prev = &t1;
+	if (input_checker(&t1) == EXIT_FAILURE)
+		return (EXIT_SUCCESS);
+	return (EXIT_FAILURE);
+}
+
+int	should_fail_for_consecutive_pipes(void)
+{
+	t_token t1 = {"ls", TOKEN_WORD, BOOL_FALSE, NULL, NULL};
+	t_token t2 = {"|", TOKEN_PIPE, BOOL_FALSE, NULL, NULL};
+	t_token t3 = {"|", TOKEN_PIPE, BOOL_FALSE, NULL, NULL};
+	t_token t4 = {"grep", TOKEN_WORD, BOOL_FALSE, NULL, NULL};
+
+	t1.next = &t2;
+	t2.prev = &t1;
+	t2.next = &t3;
+	t3.prev = &t2;
+	t3.next = &t4;
+	t4.prev = &t3;
+	if (input_checker(&t1) == EXIT_FAILURE)
+		return (EXIT_SUCCESS);
+	return (EXIT_FAILURE);
+}
+
+int	should_pass_for_valid_pipe(void)
+{
+	t_token t1 = {"ls", TOKEN_WORD, BOOL_FALSE, NULL, NULL};
+	t_token t2 = {"|", TOKEN_PIPE, BOOL_FALSE, NULL, NULL};
+	t_token t3 = {"grep", TOKEN_WORD, BOOL_FALSE, NULL, NULL};
+
+	t1.next = &t2;
+	t2.prev = &t1;
+	t2.next = &t3;
+	t3.prev = &t2;
+	if (input_checker(&t1) == EXIT_SUCCESS)
+		return (EXIT_SUCCESS);
+	return (EXIT_FAILURE);
 }
 
 int	main(void)
 {
 	RUN_TEST(shoud_fail_for_pipe_only_prompt);
+	RUN_TEST(should_fail_for_pipe_at_start);
+	RUN_TEST(should_fail_for_pipe_at_end);
+	RUN_TEST(should_fail_for_consecutive_pipes);
+	RUN_TEST(should_pass_for_valid_pipe);
 	return (0);
 }
